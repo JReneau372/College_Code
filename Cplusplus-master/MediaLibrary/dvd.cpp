c0f997edd9da48f3e0b54f2b3ec0d2379847b0ca
Jacob M Reneau <32472559+JReneau372@users.noreply.github.com>
2018-01-20 14:45:55 -0600

    Update dvd.cpp

--- Cplusplus-master/MediaLibrary/dvd.cpp
+++ Cplusplus-master/MediaLibrary/dvd.cpp
@@ -1,12 +1,8 @@
 // Name: Jacob Reneau
 //
-// Date: 10/24/24
-//
-//
 // Desription: This program uses classes and class inheritance to build a media library
 //
-//
-//
+
 
 #include <iostream>
 #include "dvd.h"
