2228c8be46c1235643dd9ee80cc6ff9a9b33b2e0
Jacob M Reneau <32472559+JReneau372@users.noreply.github.com>
2018-01-20 14:42:35 -0600

    Update rational.h

--- Cplusplus-master/RationalNumbers/rational.h
+++ Cplusplus-master/RationalNumbers/rational.h
@@ -1,13 +1,7 @@
 // Name: Jacob Reneau
 //
-// Date: 11/7/14
-//
-//
 // Description: This program creates a custom class for rational numbers and allows for overloading of operators.
 //
-//
-//
-
 
 #ifndef RATIONAL_H
 #define RATIONAL_H
