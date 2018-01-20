7c7ae29b62edeaf065d15c84c1ee420270e0359a
Jacob M Reneau <32472559+JReneau372@users.noreply.github.com>
2018-01-20 14:41:38 -0600

    Update main.cpp

--- Cplusplus-master/RationalNumbers/main.cpp
+++ Cplusplus-master/RationalNumbers/main.cpp
@@ -1,13 +1,7 @@
 // Name: Jacob Reneau
 //
-// 
-//
-//
 // Description: This program creates a custom class for rational numbers and allows for overloading of operators.
 //
-//
-//
-
 
 #include <iostream>
 #include <ostream>
@@ -19,7 +13,7 @@ int main(){
     rational x (3,4);   // this represents the fraction 3/4
     rational y (1,4);	// this represents the fraction 2/4
     rational z;
-    
+
     cout<< "x is: ";    // this prints the fraction 3/4 to the screen
     x.print();
     cout<<endl;
@@ -28,11 +22,9 @@ int main(){
     y.print();
     cout<<endl;
     
-    
     // adding fractions:  3/4 + 2/4 would equal 5/4
     
     z=x+y;
-    
     
     cout<< "Adding x and y produces z = ";
     z.print();
@@ -40,7 +32,6 @@ int main(){
     
     x.print();
     y.print();
-    
     
     // subtracting fractions:  3/4 - 2/4 would equal 1/4
     
@@ -52,13 +43,11 @@ int main(){
     
     // Multiplying fractions:  3/4 * 2/4 would equal 6/16
     
-    
     z=x*y;
     
     cout<< "Multiplying x and y produces z = ";
     z.print();
     cout<<endl;
-    
     
     // Dividing fractions:  3/4 divided by 2/4 would equal 12/8
     
@@ -82,8 +71,6 @@ int main(){
     
     
     return 0;
-    
-    
-    
+   
     
 }
